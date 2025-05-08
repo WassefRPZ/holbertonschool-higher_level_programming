bea16d17e2b342f8d4ff8ee0a12d798f8462332d
WassefRPZ <10422@holbertonstudents.com>
2025-05-08 18:32:10 +0200

    Fix capitalization in output messages in 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ if number < 0:
     last_dig = -last_dig
 
 if last_dig > 5:
-    print(f"last digit of {number} is {last_dig} and is greater than 5")
+    print(f"Last digit of {number} is {last_dig} and is greater than 5")
 
 elif last_dig == 0:
-    print(f"last digit of {number} is {last_dig} and is 0")
+    print(f"Last digit of {number} is {last_dig} and is 0")
 else:
-    print(f"last digit of {number} is {last_dig} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
