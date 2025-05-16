7cd3ab56c80c1d3cd488e0aa19938ff5ea9a9901
WassefRPZ <10422@holbertonstudents.com>
2025-05-16 21:21:21 +0200

    Remove unused uniq_add function and implement common_elements function to return common elements of two sets

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    result = 0
-    new_set = set(my_list)
-    for i in new_set:
-        result += i
-    return result
+def common_elements(set_1, set_2):
+    return set_1 & set_2
