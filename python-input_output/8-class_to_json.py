8a31e2fc000248dc2e4a25ecad5f27a2fecaa5ba
WassefRPZ <10422@holbertonstudents.com>
2025-06-03 15:54:18 +0200

    Fix typo in docstring of class_to_json function for clarity

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,7 +6,7 @@ Module contenant une fonction de conversion objet vers dictionnaire.
 
 def class_to_json(obj):
     """
-    Retourne la représentation dictionnaire d'un objet pour la sérialisation JSON.
+    Retourne la représentation dict d'un objet pour la sérialisation JSON.
 
     Args:
         obj: Instance d'une classe à convertir.
