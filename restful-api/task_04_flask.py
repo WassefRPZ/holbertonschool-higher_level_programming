f7695bd0e3ea053406f11b13efeb0710e1eeee1a
WassefRPZ <10422@holbertonstudents.com>
2025-06-13 12:39:33 +0200

    Refactor code formatting for improved readability and consistency

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -7,17 +7,21 @@ app = Flask(__name__)
 
 users = {}
 
+
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+
 
 @app.route("/data")
 def get_data():
     return jsonify(list(users.keys()))
 
+
 @app.route("/status")
 def status():
     return "OK"
+
 
 @app.route("/users/<username>")
 def get_user(username):
@@ -25,19 +29,21 @@ def get_user(username):
         return jsonify(users[username])
     else:
         return jsonify({"error": "User not found"})
-    
+
+
 @app.route("/add_user", methods=["POST"])
 def add_user():
     data = request.get_json()
-    
+
     if not data or "username" not in data:
         return jsonify({"error": "Username is required"}), 400
 
     username = data["username"]
-    
+
     users[username] = data
-    
-    return jsonify({"message": "User added","user": data}), 201
+
+    return jsonify({"message": "User added", "user": data}), 201
+
 
 if __name__ == "__main__":
     app.run()
