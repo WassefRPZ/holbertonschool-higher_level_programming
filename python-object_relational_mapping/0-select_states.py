9d79914eec7baca292c996a412ed62c5d4ae3d2d
WassefRPZ <10422@holbertonstudents.com>
2025-06-27 20:29:30 +0200

    Fix formatting and add newline at end of file in select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,9 +2,11 @@
 """ Script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3])
@@ -13,3 +15,4 @@ if __name__ == "__main__":
     rows = cur.fetchall()
     for row in rows:
         print(row)
+    
