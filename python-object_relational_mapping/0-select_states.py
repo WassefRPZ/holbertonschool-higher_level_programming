bd6fb2681daf6c969c16d41d5e3a446006641b0e
WassefRPZ <10422@holbertonstudents.com>
2025-06-27 20:40:16 +0200

    Fix file permissions, add newline at end of file, and ensure proper resource closure in select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Script that lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
 import sys
 
@@ -16,3 +17,5 @@ if __name__ == "__main__":
         print(row)
     cursor.close()
     db.close()
+
+    
