157b423665e094b3c3cf147e44ee90b8e4cc69a9
WassefRPZ <10422@holbertonstudents.com>
2025-06-27 22:02:34 +0200

    Remove unnecessary comments and imports in model_state.py for cleaner code

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-""" Module that contains the State class """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import MetaData
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ Class that represents a state """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
