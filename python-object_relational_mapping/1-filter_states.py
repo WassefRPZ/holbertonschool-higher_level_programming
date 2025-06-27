241b70bdef867e2467b24515b7f93c891b8d1d87
WassefRPZ <10422@holbertonstudents.com>
2025-06-27 21:28:37 +0200

    Refactor SQL query formatting and ensure proper resource closure in filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,15 +2,22 @@
 """ Script that lists all states with a name starting with """
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3])
+        db=sys.argv[3]
+    )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states " \
-    "WHERE BINARY name LIKE 'N%' ORDER BY id ASC;")
+    cur.execute(
+        "SELECT * FROM states "
+        "WHERE name LIKE BINARY 'N%' "
+        "ORDER BY id ASC;"
+    )
     rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
