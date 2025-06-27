4dc9a1e1fb62a40dce8211ced3dda5fd8faaec28
WassefRPZ <10422@holbertonstudents.com>
2025-06-27 21:13:15 +0200

    Fix formatting of SQL query in filter_states.py for improved readability

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3])
     cur = db.cursor()
-    sql = "SELECT cities.id, cities.name, states.name FROM cities\
-        INNER JOIN states ON cities.state_id = states.id"
+    sql = "SELECT cities.id, cities.name, states.name FROM cities \
+        INNER JOIN states ON cities.state_id = states.id \
+        ORDER BY cities.id ASC"
     cur.execute(sql)
     rows = cur.fetchall()
     for row in rows:
--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that lists all cities from the database """
+""" Script that lists all cities of a state from the database """
 import MySQLdb
 import sys
 if __name__ == "__main__":
@@ -9,10 +9,9 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3])
     cur = db.cursor()
-    sql = "SELECT cities.name FROM cities\
-        INNER JOIN states ON cities.state_id = states.id\
-            WHERE states.name = %s"\
-            "ORDER BY cities.id"
+    sql = "SELECT cities.name FROM cities \
+        INNER JOIN states ON cities.state_id = states.id \
+        WHERE states.name = %s ORDER BY cities.id"
     cur.execute(sql, (sys.argv[4],))
     rows = cur.fetchall()
     print(", ".join([row[0] for row in rows]))
