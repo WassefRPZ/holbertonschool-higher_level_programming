e8537c35e029006a84715c022cfe4cd84808eaca
WassefRPZ <10422@holbertonstudents.com>
2025-06-27 20:35:30 +0200

    Refactor cursor variable naming and ensure proper resource closure in select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
         print(row)
-    cur.close()
+    cursor.close()
+    db.close()
