b80518d6ec76921b98c5675770cff843cb61ab3e
WassefRPZ <10422@holbertonstudents.com>
2025-06-27 20:49:53 +0200

    Fix missing newline at end of file and ensure proper resource closure in filter states scripts

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,8 @@
-""" Script that lists all states with a name starting with """
+#!/usr/bin/python3
+""" Script that lists all states where name matches the argument """
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
@@ -8,9 +10,10 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE CONVERT(name USING Latin1)\
-                 COLLATE Latin1_General_CS = '{}'\
-                 ORDER BY states.id".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
