e1fb96fbc258a721f8bc4c68df420e61be6f6f59
WassefRPZ <10422@holbertonstudents.com>
2025-09-05 17:54:40 +0200

    Add calculation script to perform basic arithmetic operations

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import calculator_1
+
+
+def main():
+    a = 10
+    b = 5
+
+    result = (a + b)
+    print("{} + {} = {}".format(a, b, result))
+
+    result = (a - b)
+    print("{} - {} = {}".format(a, b, result))
+
+    result = (a * b)
+    print("{} * {} = {}".format(a, b, result))
+
+    result = (a / b)
+    print("{} / {} = {}".format(a, b, result))
+
+
+if __name__ == "__main__":
+    main()
