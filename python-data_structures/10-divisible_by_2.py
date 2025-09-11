50dc83ef75945ac6dde121fc50235f5223c826fb
WassefRPZ <10422@holbertonstudents.com>
2025-09-11 15:26:47 +0200

    Fix spacing and formatting issues in multiple files

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     result = []
     for i in my_list:
-        if i %2 == 0:
+        if i % 2 == 0:
             result.append(True)
         else:
             result.append(False)
