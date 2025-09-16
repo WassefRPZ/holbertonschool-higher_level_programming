0b1aa4c030cb7c4f40cc117b80751dad1dc2e789
WassefRPZ <10422@holbertonstudents.com>
2025-09-16 10:12:45 +0200

    Add docstring to 0-square.py for class Square definition

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Ce fichier définit une classe Square vide.
+"""
+
+
 class Square:
-    
+    """
+    Classe qui représente un carré.
+    Pour l’instant, elle ne contient aucun attribut ni méthode.
+    """
     pass
