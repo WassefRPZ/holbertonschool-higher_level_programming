111601d00e729ffb2bfea2933fe8d94c7991d12c
WassefRPZ <10422@holbertonstudents.com>
2025-10-03 21:22:11 +0200

    Add module docstring to read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that contains a function to read a file"""
+
+
 def read_file(filename=""):
     """Lit un fichier texte et affiche son contenu."""
     with open(filename, "r", encoding="utf-8") as f:
