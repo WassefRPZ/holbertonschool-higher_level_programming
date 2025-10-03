94bb9047de5831ba674f9d66746faf0f22bdd5c3
WassefRPZ <10422@holbertonstudents.com>
2025-10-03 21:31:44 +0200

    Refactor 7-add_item.py to improve structure and maintainability

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Script qui ajoute les arguments passés en ligne de commande
+dans une liste, et sauvegarde cette liste dans un fichier JSON.
+"""
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -6,14 +10,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-"""Script qui ajoute les arguments passés en ligne de commande
-dans une liste, et sauvegarde cette liste dans un fichier JSON.
-"""
+if __name__ == "__main__":
+    try:
+        items = load_from_json_file(filename)
+    except Exception:
+        items = []
 
-try:
-    item = load_from_json_file(filename)
-except Exception:
-    item = []
-
-item.extend(sys.argv[1:])
-save_to_json_file(item, filename)
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
