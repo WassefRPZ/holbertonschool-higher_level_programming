5fab6fc8c45b62689fe71d8629a205ed80a880e9
WassefRPZ <10422@holbertonstudents.com>
2025-10-24 19:18:18 +0200

    Add comments to clarify user privileges in SQL script

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
+-- that lists all privileges of the
+-- MySQL users user_0d_1 and user_0d_2
 CREATE USER if NOT EXISTS user_0d_1@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@'localhost';
